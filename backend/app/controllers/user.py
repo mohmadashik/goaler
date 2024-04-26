a0c64e71839dcdb4160067119a6c422f96183151
amohmad <amohmad@monarchtractor.com>
2024-04-26 11:42:18 +0530

    frontend app

--- backend/app/controllers/user.py
+++ backend/app/controllers/user.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, send_from_directory
 user_bp = Blueprint('user_controller',__name__,url_prefix='/user')
 
 
 @user_bp.route('/')
 def index():
+    # build_dir = os.path.join(os.path.dirname(__file__),'..','..','frontend','build')
+    # print(build_dir)
+    # return send_from_directory(build_dir,'index.html')
     return 'User controller is UP!'
---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+// src/components/App.js
+
+import React from 'react';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Goaler App is UP!</h1>
+        </div>
+    )
+}
+
+export default App;
