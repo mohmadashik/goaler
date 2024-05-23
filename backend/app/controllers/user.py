c1a6db8f4bed968a3230037f3f724dde03ac6dbd
amohmad <amohmad@monarchtractor.com>
2024-05-23 16:25:31 +0530

    frontend changes

--- backend/app/controllers/user.py
+++ backend/app/controllers/user.py
@@ -8,4 +8,8 @@ def index():
     # build_dir = os.path.join(os.path.dirname(__file__),'..','..','frontend','build')
     # print(build_dir)
     # return send_from_directory(build_dir,'index.html')
-    return 'User controller is UP!'
+    return 'User controller is UPPPPPP!'
+
+@user_bp.route('/login')
+def login():
+    return 'USER LOGIN PAGE'
--- backend/start.sh
+++ backend/start.sh
@@ -6,3 +6,4 @@ source ../venv/bin/activate
 gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app --daemon &
 
 echo "Backend Server Started"
+echo "http://localhost:5000/"
