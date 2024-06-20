5438c899509ea78d5d13c4f54f0f3848e4401e91
amohmad <amohmad@monarchtractor.com>
2024-06-20 09:25:38 +0530

    login, register, dashboard

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,73 @@
 // src/components/Home.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import homeImage from './images/homeImage.jpg'; // Ensure you have an image in the src/components directory
 
 const Home = () => {
+  const [animate, setAnimate] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  const handleLogin = () => {
+    navigate('/login');
+  }
+
+  const handleSignup = () => {
+    navigate('/register');
+  }
+
   return (
-    <div>
-      <h1>Welcome to Goaler App</h1>
-      <h2>Here you don't just set Goals</h2>
-      <h1>But ACHIEVE THEM!</h1>
-    </div>
+    <Container>
+      <Box 
+        display="flex" 
+        alignItems="center" 
+        justifyContent="center" 
+        mt={4} 
+        sx={{ height: '100vh', textAlign: 'center' }}
+      >
+        <Box
+          className={animate ? 'bounce' : ''}
+          alignItems="left"
+          component="img"
+          src={homeImage}
+          alt="Welcome"
+          sx={{ 
+            width: '80%', 
+            height: 'auto', 
+            borderRadius: '10%', 
+            marginRight: '10%',
+            boxShadow: 30
+          }}
+        />
+        <Box 
+          className={animate ? 'bounce' : ''} 
+          sx={{ 
+            width: '45%', 
+            display: 'flex', 
+            flexDirection: 'column', 
+            justifyContent: 'right', 
+            alignItems: 'right' 
+          }}
+        >
+          <Typography variant="h1" component="h1" gutterBottom>
+            Welcome to Goaler App
+          </Typography>
+          <Typography variant="h2" component="h2" gutterBottom>
+            where you don't just set the goals,
+            but <br></br>
+            ACHIEVE THEM!!!
+          </Typography>
+          <div className="mt-4">
+            <button className="btn btn-primary mr-2" onClick={handleLogin} >Login</button>
+            <button className="btn btn-secondary" onClick={handleSignup} >Signup</button>
+          </div>        
+          </Box>
+      </Box>
+    </Container>
   );
 };
 
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import {Container,Box} from '@mui/material';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -33,31 +34,46 @@ const Register = () => {
   });
 
   return (
+    <Container>
+    <Box 
+      display="flex" 
+      alignItems="center" 
+      justifyContent="center" 
+      mt={4} 
+      sx={{ height: '100vh', textAlign: 'left' }}
+    >
     <form onSubmit={formik.handleSubmit}>
-      <div>
+      <h1>Register</h1>
+      <br></br>
+      <div class="form-group">
         <label htmlFor="username">Username</label>
         <input
           id="username"
           type="text"
+          class="form-control"
           {...formik.getFieldProps('username')}
         />
         {formik.touched.username && formik.errors.username ? (
           <div>{formik.errors.username}</div>
         ) : null}
       </div>
-      <div>
+      <div class="form-group">
         <label htmlFor="password">Password</label>
         <input
           id="password"
           type="password"
+          class="form-control"
           {...formik.getFieldProps('password')}
         />
         {formik.touched.password && formik.errors.password ? (
           <div>{formik.errors.password}</div>
         ) : null}
       </div>
-      <button type="submit" disabled={formik.isSubmitting}>Register</button>
+      <button type="submit" class='btn btn-primary' disabled={formik.isSubmitting}>Register</button>
     </form>
+    </Box>
+
+    </Container>
   );
 };
 
